8a6932f5e585f0da96a62187e7fe237f5462ef10
realview45 <kjink0125@gmail.com>
2026-02-02 00:03:48 +0900

    로그인

--- src/main/java/com/example/oauth/member/controller/MemberController.java
+++ src/main/java/com/example/oauth/member/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.example.oauth.member.controller;
 
+import com.example.oauth.common.auth.JwtTokenProvider;
 import com.example.oauth.member.domain.Member;
 import com.example.oauth.member.dto.MemberCreateDto;
+import com.example.oauth.member.dto.MemberLoginDto;
+import com.example.oauth.member.dto.MemberLoginResDto;
 import com.example.oauth.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,13 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
     @Autowired
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
     @PostMapping("/create")
     public ResponseEntity<?> memberCreate(@RequestBody MemberCreateDto dto){
         Member member = memberService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(member.getId());
+    }
+    @PostMapping("/doLogin")
+    public MemberLoginResDto login(@RequestBody MemberLoginDto dto){
+        Member member = memberService.login(dto);
+        String accessToken = jwtTokenProvider.createToken(member);
+        return MemberLoginResDto.builder()
+                .access_token(accessToken)
+                .refresh_token(null).build();
     }
 }
--- src/main/java/com/example/oauth/member/dto/MemberLoginDto.java
+++ src/main/java/com/example/oauth/member/dto/MemberLoginDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class MemberCreateDto {
+public class MemberLoginDto {
     private String email;
     private String password;
 
